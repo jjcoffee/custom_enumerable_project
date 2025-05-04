86731f77be08d3b641127a2f5f02e73eb1c4bce8
Joel Davies <joeld.dev@gmail.com>
2025-05-04 11:20:21 +0200

    feat: add custom each method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,5 @@
 module Enumerable
-  # Your code goes here
+
 end
 
 # You will first have to define my_each
@@ -7,5 +7,9 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for item in self do
+      yield(item)
+    end
+  end
 end
